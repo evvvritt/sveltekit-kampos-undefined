b4d6fbfd978a98bf0fbc9209dfbca0be309502b4
Everett Williams <evv.info@gmail.com>
2022-10-07 14:05:14 +0000

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,3 +6,10 @@
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>
+<small><span style="font-family:monospace">src/routes/+page.svelte</span></small>
+</p>
+<pre>
+kampos: { Kampos }
+</pre>
