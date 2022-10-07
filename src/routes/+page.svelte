1bbcd5549f824001c7e54c0059649c4e2c6e8678
evvvritt <evv.info@gmail.com>
2022-10-07 16:01:01 +0200

    init sveltekit, import kamps (undefined)

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { Kampos } from 'kampos'
+  // created src/kampos.d.ts suppress type warning
+  console.log(Kampos) // undefined :(
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
